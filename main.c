14b5cfe5f26c45ed8a116780e8b3da04187f45a7
Alexande Becker <becker.alexander42@gmail.com>
2021-03-30 15:56:00 +0200

    tried something for the not working pwm

--- main.c
+++ main.c
@@ -20,14 +20,15 @@ int main() {
 	adc_init();
 	adc_gpio_init(POT_IN);
 	adc_select_input(0);
-	uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
-	pwm_set_wrap(slice_num, 1024);
-	pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);
-	pwm_set_enabled(slice_num, true);
+	uint slice = pwm_gpio_to_slice_num(LED_PIN);
+	uint channel = pwm_gpio_to_channel(LED_PIN);
+	pwm_set_wrap(slice, 1024);
+	pwm_set_chan_level(slice, channel, 0);
+	pwm_set_enabled(slice, true);
 	while (true) {
 		uint16_t adc_result = adc_read();
 		printf("Raw value: 0x%03x, shifted value: 0x%03x voltage: %f V\n", adc_result, (adc_result >> 6), adc_result * conversion_factor);
-		pwm_set_chan_level(slice_num, PWM_CHAN_A, (adc_result >> 6));
+		pwm_set_chan_level(slice_num, channel, (adc_result >> 6));
 		sleep_ms(500);
 	}
 }

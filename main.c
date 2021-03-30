79b3d978663b8ac9760c4f449e7d6e2cb3946cf7
Alexande Becker <becker.alexander42@gmail.com>
2021-03-30 15:43:40 +0200

    fixed a few more bugs

--- main.c
+++ main.c
@@ -16,8 +16,8 @@ int main() {
 
 	stdio_init_all();
 
-	gpio_init(LED_PIN);
 	gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+	adc_init();
 	adc_gpio_init(POT_IN);
 	adc_select_input(0);
 	uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
@@ -26,8 +26,8 @@ int main() {
 	pwm_set_enabled(slice_num, true);
 	while (true) {
 		uint16_t adc_result = adc_read();
-		printf("Raw value: 0x%03x, voltage: %f V/n", adc_result, adc_result * conversion_factor);
-		pwm_set_chan_level(slice_num, PWM_CHAN_A, adc_result);
+		printf("Raw value: 0x%03x, shifted value: 0x%03x voltage: %f V\n", adc_result, (adc_result >> 6), adc_result * conversion_factor);
+		pwm_set_chan_level(slice_num, PWM_CHAN_A, (adc_result >> 6));
 		sleep_ms(500);
 	}
 }
